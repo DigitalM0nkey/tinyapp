7b12eb5fab90dd8f6be21fbe327efb2bbe96f64c
Terrence Kuiper <github@terrencekuiper.com>
2019-09-30 18:37:46 -0700

    Save user entered urls to the urlDatabase

--- express_server.js
+++ express_server.js
@@ -16,6 +16,9 @@ const urlDatabase = {
 
 app.post("/urls", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
+  urlDatabase[generateRandomString()] = req.body.longURL;
+  console.log(urlDatabase);
+
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
