24b684efd3530773182f252693cc48676d0cc6de
Terrence Kuiper <github@terrencekuiper.com>
2019-10-01 19:12:50 -0700

    Tidying up

--- express_server.js
+++ express_server.js
@@ -67,7 +67,8 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 
